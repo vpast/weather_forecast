6409e4c106ae0d331332d30010ef28330a8f580b
Sweet Heaven <dalverq@gmail.com>
2021-04-18 00:11:27 +0300

    pages and components

---
+++ components/Input.js
@@ -0,0 +1,9 @@
+const InputComponent = () => {
+  return (
+    <div>
+      <input value="Type your city"></input>
+    </div>
+  )
+}
+
+export default InputComponent
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import InputComponent from "../components/Input"
+
+const IndexPage = () => {
+  return (
+    <InputComponent />
+  )
+}
+
+export default IndexPage
