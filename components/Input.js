3bbe987736d8d53eeb9db00ffbd5f79af5710a5b
Sweet Heaven <dalverq@gmail.com>
2021-04-19 18:22:41 +0300

    input enter key

--- components/Input.js
+++ components/Input.js
@@ -2,7 +2,11 @@ const InputComponent = ({setCity}) => {
   return (
     <div className="input_flex">
       <p>Your city</p>
-      <input type="text" placeholder="London" onChange={(e) => {setCity(e.target.value)}}></input>
+      <input type="text" placeholder="London" onKeyPress={(e) => {
+        if (e.key === 'Enter') {
+          setCity(e.target.value)
+        }
+      }}></input>
     </div>
   )
 }
