8da957e2f8dd096f196a3a594b7afae4b3141540
Sweet Heaven <dalverq@gmail.com>
2021-06-29 16:08:49 +0300

    forrmat document

--- components/WeatherChart.js
+++ components/WeatherChart.js
@@ -1,20 +1,20 @@
-import Chart, { BubbleController } from 'chart.js/auto';
+import Chart, { BubbleController } from "chart.js/auto";
 import { useRef, useEffect, useState } from "react";
 
-const WeatherChart = ({data, activeDay}) => {
+const WeatherChart = ({ data, activeDay }) => {
   const [myChart, setMyChart] = useState(null);
   const chartRef = useRef(null);
 
   useEffect(() => {
     if (!chartRef.current) return;
 
-    const temp = []
+    const temp = [];
 
     for (let i = 0; i <= 3; i++) {
-      temp.push(Math.round(data.daily[i].temp.day))
+      temp.push(Math.round(data.daily[i].temp.day));
     }
 
-    const ctx = chartRef.current.getContext('2d');
+    const ctx = chartRef.current.getContext("2d");
     const myChart = new Chart(ctx, {
       type: "mixed",
       data: {
@@ -22,25 +22,27 @@ const WeatherChart = ({data, activeDay}) => {
         datasets: [
           {
             type: "bubble",
-            label: 'Day',
-            data: [{
-              x: 1,
-              y: Math.round(data.daily[0].temp.day),
-              r: 5
-            }],
-            backgroundColor: 'rgb(255, 99, 132)'
+            label: "Day",
+            data: [
+              {
+                x: 1,
+                y: Math.round(data.daily[0].temp.day),
+                r: 5,
+              },
+            ],
+            backgroundColor: "rgb(255, 99, 132)",
           },
           {
             type: "line",
             label: "Temperature Day",
             data: temp,
-            cubicInterpolationMode: 'monotone',
+            cubicInterpolationMode: "monotone",
             fill: true,
-            borderColor: 'rgb(75, 192, 192)',
-            backgroundColor: 'rgb(189, 255, 255)',
-            tension: 0.2
-          }
-      ],
+            borderColor: "rgb(75, 192, 192)",
+            backgroundColor: "rgb(189, 255, 255)",
+            tension: 0.2,
+          },
+        ],
       },
       options: {
         responsive: true,
@@ -50,33 +52,33 @@ const WeatherChart = ({data, activeDay}) => {
       },
     });
     setMyChart(myChart);
-  }, [chartRef])
+  }, [chartRef]);
 
   useEffect(() => {
     if (!myChart) return;
-    const temp = []
+    const temp = [];
 
     for (let i = 0; i <= 3; i++) {
-      temp.push(Math.round(data.daily[i].temp.day))
+      temp.push(Math.round(data.daily[i].temp.day));
     }
     myChart.data.datasets[1].data = temp;
     myChart.update();
-  }, [data])
+  }, [data]);
 
   useEffect(() => {
     if (!myChart) return;
     if (activeDay === null) return;
-    myChart.data.datasets[0].data =[{
-      x: activeDay + 1,
-      y: Math.round(data.daily[activeDay].temp.day),
-      r: 5
-    }];
+    myChart.data.datasets[0].data = [
+      {
+        x: activeDay + 1,
+        y: Math.round(data.daily[activeDay].temp.day),
+        r: 5,
+      },
+    ];
     myChart.update();
-  }, [activeDay])
-  
-  return (
-    <canvas ref={chartRef}></canvas>
-  );
+  }, [activeDay]);
+
+  return <canvas ref={chartRef}></canvas>;
 };
 
 export default WeatherChart;
