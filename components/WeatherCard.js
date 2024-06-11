8b171744e0f5a3674b2132e5c2ce5a70dd919bb3
vpast <vpast.dev@gmail.com>
2024-06-11 12:51:40 +0200

    useMemo

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,27 +1,32 @@
-import { getFormattedDate, getNextDate } from '../utils/utils'
-import WeatherItem from './WeatherItem'
+import { useMemo } from 'react';
+import { getFormattedDate, getNextDate } from '../utils/utils';
+import WeatherItem from './WeatherItem';
 
 const WeatherCard = ({ data, currentDate, activeDay, setActiveDay }) => {
   const step = 8;
-  const days = [];
 
-  days.push({
-    itemTitle: 'Today',
-    src: `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`,
-    humidity: data.list[0].main.humidity,
-  });
+  const days = useMemo(() => {
+    const daysArr = [];
 
-  for (let i = 8; i <= 37; i+= step) {
-    const date = getNextDate(currentDate, i / step);
-    const formattedDate = getFormattedDate(date);
-    if (data.list[i] != undefined) {
-      days.push({
-        itemTitle: formattedDate,
-        src: `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`,
-        humidity: data.list[i].main.humidity,
-      });
+    daysArr.push({
+      itemTitle: 'Today',
+      src: `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`,
+      humidity: data.list[0].main.humidity,
+    });
+    for (let i = 8; i <= 37; i += step) {
+      console.log('Boom');
+      const date = getNextDate(currentDate, i / step);
+      const formattedDate = getFormattedDate(date);
+      if (data.list[i] != undefined) {
+        daysArr.push({
+          itemTitle: formattedDate,
+          src: `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`,
+          humidity: data.list[i].main.humidity,
+        });
+      }
     }
-  }
+    return daysArr;
+  }, [data, currentDate]);
 
   return (
     <>
